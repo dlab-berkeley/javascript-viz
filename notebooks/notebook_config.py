7aa4068f9d2f1aecf9370ddba17fa82200040f60
Dav Clark <davclark@berkeley.edu>
2014-03-05 10:16:15 -0800

    Added commands to run ipython on server

---
+++ notebooks/notebook_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+
+# Notebook config
+c.NotebookApp.certfile = u'/home/collaboratool/collaboratool-cert.pem'
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'a8fbc05cbb069b5651205ecc82ecf38df895e270'
