d5fe688f00e4ffc69c9b1359eb20bde55ff0e059
Dav Clark <davclark@berkeley.edu>
2014-03-05 10:20:57 -0800

    fixed passwd

--- notebooks/notebook_config.py
+++ notebooks/notebook_config.py
@@ -4,4 +4,4 @@ c = get_config()
 c.NotebookApp.certfile = u'/home/collaboratool/collaboratool-cert.pem'
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'a8fbc05cbb069b5651205ecc82ecf38df895e270'
+c.NotebookApp.password = u'sha1:2676feb3efb2:f57c071b35237f5c33aafa7c98a8c4990b775414'
